bf944d51f9585f42caada5971a907f93e1b42fec
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-05-27 17:16:14 -0300

    Faculdade

---
+++ aula05/HelloWorld.java
@@ -0,0 +1,6 @@
+package aula05;
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ aula06/argumentos.java
@@ -0,0 +1,7 @@
+class argumentos {
+
+    public static void main (String [] args) {
+
+        System.out.println("Você digitou " + args[0]);
+    }
+}
